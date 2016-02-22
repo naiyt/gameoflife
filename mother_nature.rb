e770498cea6c018cb00fa1aea0052c904e9ce5d5
Nate Collings <nate@natecollings.com>
2016-02-21 17:27:36 -0700

    Potential neighbors

--- mother_nature.rb
+++ mother_nature.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-nav'
+
 class Cell < Struct.new(:x, :y)
 end
 
@@ -7,13 +10,17 @@ class MotherNature
   def initialize(grid_size, initial_cells)
     @grid  = Array.new(grid_size) { Array.new(grid_size) }
     @cells = initial_cells
-    @cells.each { |c| @grid[c.x][c.y] = c }
+    update_grid!
   end
 
   def update_world
     new_cells = []
 
     @cells.each do |cell|
+      potential_neighbors_of(cell).each do |neighbor|
+        new_cells << Cell.new(neighbor.x, neighbor.y) if neighbor_count_of(neighbor) == 3
+      end
+
       if survived?(cell)
         @new_cells << cell
       else
@@ -22,6 +29,11 @@ class MotherNature
     end
 
     @cells = new_cells
+    update_grid!
+  end
+
+  def update_grid!
+    @cells.each { |c| @grid[c.x][c.y] = c }
   end
 
   private
@@ -36,7 +48,24 @@ class MotherNature
   end
 
   def neighbors_of(cell)
-    [top_left(cell), top_middle(cell), top_right(cell), middle_left(cell), middle_right(cell), bottom_left(cell), bottom_middle(cell), bottom_right(cell)]
+    [
+      top_left(cell),    top_middle(cell),    top_right(cell),
+      middle_left(cell),                      middle_right(cell),
+      bottom_left(cell), bottom_middle(cell), bottom_right(cell)
+    ]
+  end
+
+  def potential_neighbors_of(cell)
+    [
+      top_left(cell)      || Cell.new(cell.x-1, cell.y-1),
+      top_middle(cell)    || Cell.new(cell.x,   cell.y-1),
+      top_right(cell)     || Cell.new(cell.x+1, cell.y-1),
+      middle_left(cell)   || Cell.new(cell.x-1, cell.y  ),
+      middle_right(cell)  || Cell.new(cell.x+1, cell.y  ),
+      bottom_left(cell)   || Cell.new(cell.x-1, cell.y+1),
+      bottom_middle(cell) || Cell.new(cell.x,   cell.y+1),
+      bottom_right(cell)  || Cell.new(cell.x+1, cell.y+1),
+    ]
   end
 
   def top_left(cell)
